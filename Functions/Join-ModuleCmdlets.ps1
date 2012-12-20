75103103c865f297cceae5f85cbec3b05e94218b
Brian Addicks <brian@addicks.us>
2012-12-19 20:08:25 -0500

    building initial file structure

---
+++ Functions/Join-ModuleCmdlets.ps1
@@ -0,0 +1,57 @@
+﻿function Join-ModuleCmdlets {
+	<#
+	.SYNOPSIS
+		synopsis
+	.DESCRIPTION
+		description
+	.PARAMETER Command
+		command
+    .PARAMETER OutDir
+		outdir
+    .EXAMPLE
+        example
+    .EXAMPLE
+        example
+	#>
+    
+    Param (
+        [Parameter(Mandatory=$True,Position=0)]
+        [String]$ModuleName,
+
+        [Parameter(Mandatory=$True,Position=1)]
+        [ValidateScript({Test-Path $_ -PathType 'Container'})] 
+        [String]$FunctionDir,
+
+        [Parameter(Mandatory=$True,Position=2)]
+        [ValidateScript({Test-Path $_ -PathType 'Container'})] 
+        [String]$OutDir,
+
+        [Parameter(Mandatory=$False)]
+        [alias('xp')]
+        [string]$ExcludePattern
+    )
+
+    BEGIN {
+        function Join-Files {
+            $Joined = @()
+            $lsparams = @{}
+            if ($ExcludePattern) { $lsparams.Add("Exclude",$ExcludePattern) }
+            $lsparams.Add("Path","$FunctionDir\*.ps1")
+
+            $Files = ls @lsparams
+
+            foreach ($File in $Files) {
+                $Current = gc $File
+                $Joined += $Current
+            }
+            
+            $Joined | Out-File "$OutDir\$ModuleName.psm1" -Force -Encoding default
+            remove-module $ModuleName -errorAction silentlyContinue
+            import-module "$OutDir\$ModuleName.psm1"
+        }
+    }
+
+    PROCESS {
+        Join-Files
+    }
+}
---
+++ Functions/inprogress.ps1
@@ -0,0 +1,81 @@
+﻿Build-PowerAlto {
+    $ExcludeCommands = @("Test-PaConnection")
+    $InProgress      = @("Update-PaSoftware")
+    $ExcludePattern  = "inprogress*"
+    $Module = "poweralto"
+    $BaseDir = "C:\Dropbox\dev\poweralto"
+    $MainHtml = "index.html"
+    $MdFile = "README.md"
+
+    cd $BaseDir
+    if ((Get-GitStatus).branch -ne "master") { "changing to master";git checkout master }
+
+    $JoinModule = @{
+        ModuleName = $Module
+        FunctionDir = "$BaseDir\Functions"
+        OutDir = $BaseDir
+        ExcludePattern = $ExcludePattern
+    }
+
+    Join-ModuleCmdlets @JoinModule
+
+
+    $HomeMd = @{
+        Module = $Module
+        OutDir = $BaseDir
+        InProgress = $InProgress
+        FileName = $MdFile
+        Header = "$BaseDir\head.md"
+        Footer = "$BaseDir\foot.md"
+        Exclude = $ExcludeCommands
+    }
+
+    Create-ModuleHomeMd @HomeMd
+
+    if ((Get-GitStatus).HasWorking) {
+        git add .
+        git commit -am "test"
+        git push
+    } else {
+        "Nothing to commit"
+    }
+
+    if ((Get-GitStatus).branch -ne "gh-pages") { "changing to gh-pages";git checkout gh-pages }
+
+    if (Test-Path "$BaseDir\$mainhtml") {
+        $HomeHtml = @{
+            Module = $Module
+            OutDir = $BaseDir
+            InProgress = $InProgress
+            FileName = $MainHtml
+            Header = "$BaseDir\parts\mainheader.html"
+            Footer = "$BaseDir\parts\mainfooter.html"
+            Exclude = $ExcludeCommands
+        }
+        "Creating Module Home Page"
+        Create-ModuleHomeHtml @HomeHtml
+
+
+        $cmdlethtml = @{
+            Command = $Module
+            OutDir = $BaseDir
+            Header = "$BaseDir\parts\cmdletheader.html"
+            Footer = "$BaseDir\parts\cmdletfooter.html"
+            Exclude = $ExcludeCommands
+        }
+        "Creating CmdletHtml"
+        Create-CmdletHtml @CmdletHtml
+
+        if ((Get-GitStatus).HasWorking) {
+            git add .
+            git commit -am "test"
+            git push
+        } else {
+            "Nothing to commit"
+        }
+    }
+
+    if ((Get-GitStatus).branch -ne "master") { "changing to master";git checkout master }
+}
+
+Set-Alias -Name bpa -Value Build-PowerAlto
